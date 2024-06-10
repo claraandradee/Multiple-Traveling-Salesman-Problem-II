a76fc91e3638021cbb8aaf61c00cb2abf78af7bc
Clara Andrade <113606213+claraandradee@users.noreply.github.com>
2024-06-10 11:27:58 -0300

    feat: method get_total_distance

--- sa.py
+++ sa.py
@@ -1,6 +1,8 @@
 # Clara Andrade Sant´anna Santos - 22124
 # Júlia Enriquetto de Brito      - 22139
 
+import math 
+
 # Lê o n caixeiros e a n cidades
 # Lê a solucao inicial
 
@@ -8,7 +10,7 @@
 # Esse metodo tem como objetivo dividir igualmente a quantidade de cidades para cada caixeiro 
 def middle_split(lst, n):
     length = len(lst)
-    div, rest = divmod(length, n) # len é o dividendo e n é o divisor - k é o resultado da divisão e m o resto
+    div, rest = divmod(length, n)  # length é o dividendo e n é o divisor - div é o resultado da divisão e rest o resto
     parts = []
     
     start = 0
@@ -20,16 +22,36 @@ def middle_split(lst, n):
     
     return parts
 
+# Exemplo de uso
+'''
 vetor = [10, 20, 25, 155, 4, 2, 0, 74, 15]
 partes = 4
 result = middle_split(vetor, partes)
 print(result)
+'''
 
 # Metodo get_distance 
+def get_total_distance(mVet):
+    add_distance = 0
+    for v in mVet:
+        distance_vector = 0
+        for i in range(1, len(v)):
+            p_previous = v[i - 1]
+            p_current = v[i]
+            distance = abs(p_current - p_previous)
+            distance_vector += distance
+        add_distance += int(distance_vector)
+    return add_distance
 
+# Exemplo de uso 
+'''
+add_distance = get_total_distance(result)
+print("A soma das distâncias dos vetores é:", add_distance)
+'''
 
 # 1. Pega a solução inicial e quebra ela - SPLIT 
 #    Após isso conta a distance total dela
+
 
 
 # 2. Com os dados acima implementaremos na heurista SA
