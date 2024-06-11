d980c6dfce68a10aea9ce9e51cec31da66ebf388
Clara Andrade <113606213+claraandradee@users.noreply.github.com>
2024-06-11 18:17:04 -0300

    feat: comentarios

---
+++ brkga.py
@@ -0,0 +1,22 @@
+# 1. 
+# formular varios tours aleatorios (100)
+# inidicar o numero da populacao que é o numero de cidades
+# indicar o numero de gerações (eu escolho e pode ser bem alto)
+
+# 2. decoder 
+# ordem as visitas (indices)
+# aqui eu coloco as distancias de cada cidade
+
+# 3. Pegar a distancia total de todas de cada caixeiro e ver a qualidade desse caminho 
+
+# 4. FITNESS PROBS = pegar os melhores caixeiros de melhor qualidade (30% das 100)
+
+# 5. Leavar esses melhores caixeiros e pegar 80% para o crossing over
+#  pegar sempre os primeiros valores de cada melhor caixeiro
+# vai ser criado um novos caixeiros(filinhos)
+
+# 6. Levar 5% dos filinhos para as mutações
+# mutação é trocar um valor aleatorio dentro do filinho por outro valor aleatorio
+
+# 7. Pegar os valores de todos os filinhos, juntar e contar sua distancia
+# vamos ter o valor final
